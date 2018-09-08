3c7a9912dddf58516653388233ea7970cbd3fbf9
StartFromThird <hzqweixin@163.com>
2018-09-08 23:14:20 +0800

    更新推荐详情页--顶部banner 及 点开后滑动展示图片的gallery

---
+++ demo04/src/pages/detail/components/Banner.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerShow">
+      <img src="http://img1.qunarzz.com/sight/p0/1509/53/5313f7a1f0386316.water.jpg_600x330_88128d16.jpg" alt="" class="banner-img">
+      <div class="banner-info">
+        <div class="banner-title">荆州楚王车马阵景区</div>
+        <div class="banner-icon"><span class="iconfont">&#xe6ee;</span> 39</div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallery" @close="handleGalleryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default{
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      imgs:["http://img1.qunarzz.com/sight/p0/1803/2e/2ee74981c5f7e0a4a3.img.jpg_r_800x800_1189225b.jpg",
+        "http://img1.qunarzz.com/sight/p0/1803/20/20bc07e39751824a3.img.jpg_350x240_05c33076.jpg"],
+      showGallery: false
+    }
+  },
+  methods: {
+    // 显示画廊
+    handleBannerShow() {
+      this.showGallery = true
+    },
+    // 隐藏画廊
+    handleGalleryClose() {
+      this.showGallery = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      position absolute
+      bottom 0
+      box-sizing border-box
+      width 100%
+      padding 0 0.2rem
+      line-height 0.6rem
+      color #fff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        float left
+        font-size 0.32rem
+      .banner-icon
+        float right
+        margin-top 0.1rem
+        padding 0 0.4rem
+        height 0.4rem
+        line-height 0.4rem
+        font-size 0.24rem
+        border-radius 0.2rem
+        background rgba(0, 0, 0, 0.8)
+        .iconfont
+          font-size 0.24rem
+</style>
