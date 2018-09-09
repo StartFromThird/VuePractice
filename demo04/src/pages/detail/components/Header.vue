0bad5ad777fdeec839cd0a40fa3cfa51b0948c00
StartFromThird <hzqweixin@163.com>
2018-09-09 21:43:54 +0800

    更新推荐详情页，顶部header
    滑动切换顶部样式，页面垂直方向滑动距离document.documentElement.scrollTop

---
+++ demo04/src/pages/detail/components/Header.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" v-show="showAbs" class="header-abs">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </router-link>
+    <div v-show="!showAbs" :style="opacityStyle" class="header-fixed">
+      景点详情
+      <router-link class="header-left" to="/">
+        <div class="iconfont fixed-back-icon">&#xe624;</div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    // 拖动页面 景点详情透明度变化及显示隐藏
+    handleScroll () {
+      // 元素的顶部到它的最顶部可见内容（的顶部）的距离的垂直度量
+      const top = document.documentElement.scrollTop
+      // console.log(top)
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = (opacity > 1) ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    // 监听滚动,这里是全局监听，会影响其他组件
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  // deactivated 页面隐藏或要替换成新页面时调用
+  deactivated () {
+    // 解绑全局事件
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+.header-abs
+  position absolute
+  top 0.2rem
+  left 0.2rem
+  width 0.8rem
+  height 0.8rem
+  line-height 0.8rem
+  border-radius 50%
+  background rgba(200, 200, 0, 0.3)
+  text-align center
+  .back-icon
+    color #fff
+.header-fixed
+  position fixed
+  top 0px
+  width 100%
+  height 0.86rem
+  background $bgColor
+  line-height 0.86rem
+  font-size 0.32rem
+  text-align center
+  color #fff
+  .header-left
+    float left
+    width: 0.64rem
+    height 0.86rem
+    // display block
+    .fixed-back-icon
+      text-align: center
+      font-size: 0.4rem
+      color #fff
+</style>
