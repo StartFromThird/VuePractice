f1eeb54f0e84bac2506a1ae17e199584a568b2b3
StartFromThird <hzqweixin@163.com>
2018-09-11 16:55:24 +0800

    异步加载组件写法

--- demo04/src/router/index.js
+++ demo04/src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from '@/components/HelloWorld'
-import Home from '@/pages/home/Home'
 // import List from '@/pages/home/List'
+// 同步写法，如果是异步写法，下面不需要了
+import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
 import Detail from '@/pages/detail/Detail'
 
@@ -15,6 +16,8 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+      // 异步写法
+      // component: () => import('@/pages/home/Home')
     }, {
       path: '/city',
       name: 'City',
@@ -24,5 +27,9 @@ export default new Router({
       name: 'Detail',
       component: Detail
     }
-  ]
+  ],
+  // 每次路由切换，页面初始位置
+  scrollBehavior (to, from, savedPosition) {
+    return {x: 0, y: 0}
+  }
 })
