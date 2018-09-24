bf8f599c7c5f2dbce0fa64a0efcae6aeea2d769d
StartFromThird <hzqweixin@163.com>
2018-09-24 23:42:29 +0800

    上下滑动公共动画方法

---
+++ demo05/src/common/SlideDown.vue
@@ -0,0 +1,24 @@
+<template>
+  <transition>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'SlideDown'
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../assets/styles/global';
+.v-enter, .v-leave-to {
+  transform: translateY(-100%);
+}
+.v-enter-to, .v-leave {
+  transform: translateY(0);
+}
+.v-enter-active, .v-leave-active {
+  transition: all 0.2s linear;
+}
+</style>
