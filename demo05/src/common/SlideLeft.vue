291513ea23d5caa5417d37eda7d5e183c61c9516
StartFromThird <hzqweixin@163.com>
2018-09-26 19:04:28 +0800

    更新公共动画

---
+++ demo05/src/common/SlideLeft.vue
@@ -0,0 +1,24 @@
+<template>
+  <transition>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'SlideLeft'
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../assets/styles/global';
+.v-enter, .v-leave-to {
+  transform: translateX(-100%);
+}
+.v-enter-to, .v-leave {
+  transform: translateX(0);
+}
+.v-enter-active, .v-leave-active {
+  transition: all 0.2s linear;
+}
+</style>
